fb70ec93ea8164ea9cae6b7b1098c33d5414e01f
mess <atichatfantasy@gmail.com>
2024-09-20 00:15:52 +0700

    fix metadata home page

--- app/(pages)/home/layout.tsx
+++ app/(pages)/home/layout.tsx
@@ -3,30 +3,8 @@ import Header from "@/common/header";
 import Footer from "@/common/footer";
 
 export const metadata: Metadata = {
-  title: {
-    default: "Atichat Thongnak",
-    template: "%s | Atichat Thongnak",
-  },
+  title: "Home",
   description: "Mess Developer, student and gamer",
-  openGraph: {
-    title: "Atichat Thongnak",
-    description: "Mess Developer, student and gamer",
-    url: "https://atichat.vercel.app/",
-    siteName: "Atichat Thongnak",
-    locale: "en_US, th_TH",
-    type: "website",
-    images: [
-      {
-        url: "https://atichat.vercel.app/og",
-        width: 1200,
-        height: 630,
-      },
-    ],
-  },
-  twitter: {
-    title: "Atichat Thongnak",
-    card: "summary_large_image",
-  },
 };
 
 export default function RootLayout({
